b211561aeaa17219f436684183eca1ad097e7129
Thomas Huang <thomas.huang2017@gmail.com>
2018-04-21 17:38:58 -0500

    Add functionality for constructing the tree with Game Collections.

--- src/game_tree.cpp
+++ src/game_tree.cpp
@@ -1,34 +1,38 @@
 #include <map>
+#include <fstream>
 #include <PGNPly.h>
+#include <PGNMove.h>
+#include <PGNMoveList.h>
+#include <PGNGame.h>
+#include <PGNGameCollection.h>
 #include "game_tree.h"
 
 using namespace pgn;
 
-chess::TreeNode::TreeNode() : ply_(Ply()), white_wins_(0), black_wins_(0), draws_(0) {
-  next_moves_ = new std::map(std::string, TreeNode);
+chess::GameTree::TreeNode::TreeNode() : ply_(Ply()), white_wins_(0), black_wins_(0), draws_(0) {
+  next_moves_ = new std::map<std::string, chess::GameTree::TreeNode*>();
 }
 
-chess::TreeNode::TreeNode(Ply ply) : ply_(ply), white_wins_(0), black_wins_(0), draws_(0) {
-  next_moves_ = new std::map(std::string, TreeNode);
+chess::GameTree::TreeNode::TreeNode(Ply ply) : ply_(ply), white_wins_(0), black_wins_(0), draws_(0) {
+  next_moves_ = new std::map<std::string, chess::GameTree::TreeNode*>();
 }
 
-chess::TreeNode::TreeNode(const TreeNode& other)
-    : ply_(other.ply_), white_wins_(other.white_wins_), black_wins_(other.black_wins_), draws_(other.draws), next_moves_(other.next_moves_) {}
+chess::GameTree::TreeNode::TreeNode(const TreeNode& other)
+    : ply_(other.ply_), white_wins_(other.white_wins_), black_wins_(other.black_wins_), draws_(other.draws_), next_moves_(other.next_moves_) {}
 
-chess::TreeNode::TreeNode(TreeNode&& other) 
-    : ply_(other.ply_), white_wins_(other.white_wins_), black_wins_(other.black_wins_), draws_(other.draws), next_moves_(other.next_moves_) {
-  other.ply_ = NULL;
+chess::GameTree::TreeNode::TreeNode(TreeNode&& other) 
+    : ply_(other.ply_), white_wins_(other.white_wins_), black_wins_(other.black_wins_), draws_(other.draws_), next_moves_(other.next_moves_) {
   other.white_wins_ = 0;
   other.black_wins_ = 0;
   other.draws_ = 0;
   other.next_moves_ = nullptr;
   }
 
-chess::TreeNode::~TreeNode() {
+chess::GameTree::TreeNode::~TreeNode() {
   clear();
 }
 
-chess::TreeNode& chess::TreeNode::operator=(const TreeNode& other) {
+chess::GameTree::TreeNode& chess::GameTree::TreeNode::operator=(const TreeNode& other) {
   if (this != &other) {
     clear();
     ply_ = other.ply_;
@@ -37,9 +41,10 @@ chess::TreeNode& chess::TreeNode::operator=(const TreeNode& other) {
     draws_ = other.draws_;
     next_moves_ = other.next_moves_;
   }
+  return *this;
 }
 
-chess::TreeNode& chess::TreeNode::operator=(TreeNode&& other) {
+chess::GameTree::TreeNode& chess::GameTree::TreeNode::operator=(TreeNode&& other) {
   if (this != &other) {
     clear();
     ply_ = other.ply_;
@@ -49,13 +54,68 @@ chess::TreeNode& chess::TreeNode::operator=(TreeNode&& other) {
     next_moves_ = other.next_moves_;
     other.clear();
   }
+  return *this;
 }
 
-void chess::TreeNode::clear() {
-  ply_ = NULL;
+void chess::GameTree::TreeNode::clear() {
   white_wins_ = 0;
   black_wins_ = 0;
   draws_ = 0;
   delete next_moves_;
+}
+
+chess::GameTree::TreeNode* chess::GameTree::TreeNode::NextPosition(const Ply& ply) {
+  if (next_moves_->find(ply.str()) == next_moves_->end()) { // Haven't seen this move before
+    next_moves_->emplace(ply.str(), new chess::GameTree::TreeNode(ply));
+  }
+  return (*next_moves_)[ply.str()];
+}
+
+void chess::GameTree::TreeNode::UpdateWins(GameResult result) {
+  if (result.isWhiteWin()) {
+    white_wins_++;
+  } else if (result.isBlackWin()) {
+    black_wins_++;
+  } else {
+    draws_++;
+  }
+}
+
+chess::GameTree::GameTree(GameCollection& games) {
+  root_ = new TreeNode();
+  for (GameCollection::iterator it = games.begin(); it != games.end(); it++) {
+    AddGame(*it);
+  }
+}
+
+chess::GameTree::GameTree(std::ifstream pgn_file) {
+  root_ = new TreeNode();
+  GameCollection collection;
+  pgn_file >> collection;
+
+  for (GameCollection::iterator it = collection.begin(); it != collection.end(); it++) {
+    AddGame(*it);
+  }
+}
+
+void chess::GameTree::AddGame(const Game& game) {
+  games_++;
+  GameResult result = game.result();
+  root_->UpdateWins(result);
+
+  chess::GameTree::TreeNode* current = root_;
+  MoveList moves = game.moves();
+
+  for (MoveList::iterator it = moves.begin(); it != moves.end(); it++) {
+    //Parse white's move.
+    Ply white_ply = it->white();
+    current = current->NextPosition(white_ply);
+    current->UpdateWins(result);
+
+    //Parse black's move.
+    Ply black_ply = it->black();
+    current = current->NextPosition(black_ply);
+    current->UpdateWins(result);
+  }
 }
 
--- src/game_tree.h
+++ src/game_tree.h
@@ -1,44 +1,53 @@
 #include <vector>
 #include <map>
-#include <PNGPly.h>
+#include <PGNPly.h>
+#include <PGNMove.h>
 #include <PGNGameCollection.h>
+#include <PGNGameResult.h>
 #include <PGNGame.h>
 
 namespace chess {
   class GameTree {
     private:
       struct TreeNode {
+        /* The ply that this TreeNode represents */
         pgn::Ply ply_;
         unsigned white_wins_;
         unsigned black_wins_;
         unsigned draws_;
-        std::map<std::string, TreeNode>* next_moves_;
+        /* All continuations that we have seen before, mapped from their algebraic notation to the next TreeNode */
+        std::map<std::string, TreeNode*>* next_moves_;
 
         TreeNode();
         TreeNode(pgn::Ply ply);
 
-        //Big 5
+        //Rule of 5
         TreeNode(const TreeNode& other);
         TreeNode(TreeNode&& other);
         ~TreeNode();
         TreeNode& operator=(const TreeNode& other);
         TreeNode& operator=(TreeNode&& other);
 
+        /* Find the child of this node given a ply. Adds to next_moves_ if necessary */
+        TreeNode* NextPosition(const pgn::Ply& ply);
+
+        void UpdateWins(pgn::GameResult result);
+
         void clear();
 
       };
-      TreeNode root_;
+      TreeNode* root_;
       unsigned games_;
 
-      TreeNode();
       /* Initialize the tree with a PNGGameCollection */
-      TreeNode(const pgn::PGNGameCollection& games);
+      GameTree(pgn::GameCollection& games);
 
       /* Initialize the tree with an input stream to a pgn file */
-      TreeNode(std::ifstream pgn_file);
+      GameTree(std::ifstream pgn_file);
 
-      /* Add a single game to this Tree */
-      void AddGame(const pgn::PGNGame& game);
+      /* Add a single game to this Tree, updating all fields. */
+      void AddGame(const pgn::Game& game);
+
 
   };
 }
